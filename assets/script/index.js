44970b512dcb3b9cf0f5da228cb4466e85adecd1
HesamHosseini <hesamhosseini.dev@gmail.com>
2023-09-23 11:32:59 +0330

    category added to project :) check for bugs

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,11 +1,96 @@
 const sortableList = document.querySelector(".sortable-list");
 const listItemWrapper = document.querySelector("#listItemWrapper");
+const categoryInput = document.getElementById("categoryInput");
+const categoryResElement = document.getElementById("categoryResElement");
+let selectedCategory = "";
+const CategorySelectOptionsWrapper = document.querySelector(
+  ".todoWrapper .categorySelector .select-options"
+);
+// set category option wrapper width
+categoryResElement.style.width = `${categoryInput.offsetWidth}px`;
+
+categoryInput.addEventListener("input", function () {
+  const inputText = categoryInput.value.toLowerCase();
+  const filteredOptions = autocompleteOptions.filter((option) =>
+    option.toLowerCase().includes(inputText)
+  );
+
+  // handle focusOut to hide options (in add event)
+  document.body.addEventListener("click", handleFocusOut);
+  function handleFocusOut(e) {
+    console.log(e.target);
+    if (e.target != categoryInput || e.target != categoryResElement) {
+      categoryResElement.style.display = "";
+      document.body.removeEventListener("click", handleFocusOut);
+    }
+  }
+
+  // Clear previous results
+  categoryResElement.innerHTML = "";
+
+  // Display matching options
+  filteredOptions.forEach((option) => {
+    const optionElement = document.createElement("div");
+    optionElement.textContent = option;
+    optionElement.addEventListener("click", function () {
+      categoryInput.value = `  دسته بندی  :` + option;
+      categoryResElement.innerHTML = ``;
+      categoryResElement.style.display = "";
+    });
+    categoryResElement.appendChild(optionElement);
+  });
+});
+
+categoryInput.addEventListener("focusin", (e) => {
+  categoryResElement.style.display = "block";
+});
+
+// select category to display
+
+function renderCategorySelectOptions(data) {
+  CategorySelectOptionsWrapper.innerHTML = ``;
+  data.forEach((categoryItem) => {
+    const newItem = document.createElement("li");
+    newItem.innerText = categoryItem;
+    newItem.addEventListener("click", handleSelectCategory);
+    CategorySelectOptionsWrapper.appendChild(newItem);
+  });
+}
+
+function toggleSelectCategory(event) {
+  const selectOptions = event.target.nextElementSibling;
+  selectOptions.style.display =
+    selectOptions.style.display === "block" ? "none" : "block";
+}
+
+function handleSelectCategory(event) {
+  const selectedOption = event.target.textContent;
+  const selectStyled = event.target.parentElement.previousElementSibling;
+  selectStyled.textContent = `دسته بندی :` + selectedOption;
+  selectedCategory = selectedOption;
+
+  selectStyled.setAttribute("value", selectedOption);
+  event.target.parentElement.style.display = "none";
+
+  renderItems();
+}
+
+document.addEventListener("click", function (event) {
+  const customSelects = document.querySelectorAll(".categorySelector");
+  customSelects.forEach(function (select) {
+    if (!select.contains(event.target)) {
+      select.querySelector(".select-options").style.display = "none";
+    }
+  });
+});
+
+// calendar Object
 const calendarObjWrapper = {
   constructor: "",
   calendar: "",
 };
 const userName = prompt("Enter you UserName pls :");
-
+const autocompleteOptions = ["عمومی"];
 async function fetchData() {
   const res = await fetch("https://63e09c6b59bb472a7425106c.mockapi.io/todos");
 
@@ -108,15 +193,16 @@ async function renderItems() {
         )
       )
     ) {
-      const li = document.createElement("li");
-      li.classList.add("item");
-      li.id = `item_${todo.id}`;
-      li.draggable = "true";
-      if (todo.extendedProps.isDone) {
-        li.classList.add("checked");
-      }
-      li.style.backgroundColor = todo.backgroundColor;
-      li.innerHTML = `
+      if (selectedCategory && todo.extendedProps.category == selectedCategory) {
+        const li = document.createElement("li");
+        li.classList.add("item");
+        li.id = `item_${todo.id}`;
+        li.draggable = "true";
+        if (todo.extendedProps.isDone) {
+          li.classList.add("checked");
+        }
+        li.style.backgroundColor = todo.backgroundColor;
+        li.innerHTML = `
        <div class="actions">
           <div class="delete"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28">
               <path fill="currentColor"
@@ -143,17 +229,75 @@ async function renderItems() {
      
       `;
 
-      li.addEventListener("dragstart", () => {
-        // Adding dragging class to item after a delay
-        setTimeout(() => li.classList.add("dragging"), 0);
-      });
-      // Removing dragging class from item on dragend event
-      li.addEventListener("dragend", () => li.classList.remove("dragging"));
-      listItemWrapper.appendChild(li);
-    }
+        li.addEventListener("dragstart", () => {
+          // Adding dragging class to item after a delay
+          setTimeout(() => li.classList.add("dragging"), 0);
+        });
+        // Removing dragging class from item on dragend event
+        li.addEventListener("dragend", () => li.classList.remove("dragging"));
+        listItemWrapper.appendChild(li);
+        if (todo.extendedProps.userName == userName) {
+          // add event to the calendar
+          calendarObjWrapper.calendar.addEvent(todo);
+        }
+      } else if (selectedCategory == "" || selectedCategory == "عمومی") {
+        const li = document.createElement("li");
+        li.classList.add("item");
+        li.id = `item_${todo.id}`;
+        li.draggable = "true";
+        if (todo.extendedProps.isDone) {
+          li.classList.add("checked");
+        }
+        li.style.backgroundColor = todo.backgroundColor;
+        li.innerHTML = `
+       <div class="actions">
+          <div class="delete"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28">
+              <path fill="currentColor"
+                d="M11.5 6h5a2.5 2.5 0 0 0-5 0ZM10 6a4 4 0 0 1 8 0h6.25a.75.75 0 0 1 0 1.5h-1.31l-1.217 14.603A4.25 4.25 0 0 1 17.488 26h-6.976a4.25 4.25 0 0 1-4.235-3.897L5.06 7.5H3.75a.75.75 0 0 1 0-1.5H10ZM7.772 21.978a2.75 2.75 0 0 0 2.74 2.522h6.976a2.75 2.75 0 0 0 2.74-2.522L21.436 7.5H6.565l1.207 14.478ZM11.75 11a.75.75 0 0 1 .75.75v8.5a.75.75 0 0 1-1.5 0v-8.5a.75.75 0 0 1 .75-.75Zm5.25.75a.75.75 0 0 0-1.5 0v8.5a.75.75 0 0 0 1.5 0v-8.5Z" />
+            </svg></div>
+          <div class="edit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                d="m5 16l-1 4l4-1L19.586 7.414a2 2 0 0 0 0-2.828l-.172-.172a2 2 0 0 0-2.828 0L5 16ZM15 6l3 3m-5 11h8" />
+            </svg>
+          </div>
+           <div class="submitEdit" >
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="currentColor" d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093l3.473-4.425a.267.267 0 0 1 .02-.022z"/></svg>          </div>
+        </div>
+        <div class="top">
+        <div class="title ">
+          ${todo.title}
+        </div>
+        <input type="checkbox" ${
+          todo.extendedProps.isDone ? "checked" : ""
+        } name="isDone" >
+      </div>
 
-    if (todo.extendedProps.userName == userName) {
-      calendarObjWrapper.calendar.addEvent(todo);
+     
+      `;
+
+        li.addEventListener("dragstart", () => {
+          // Adding dragging class to item after a delay
+          setTimeout(() => li.classList.add("dragging"), 0);
+        });
+        // Removing dragging class from item on dragend event
+        li.addEventListener("dragend", () => li.classList.remove("dragging"));
+        listItemWrapper.appendChild(li);
+        if (todo.extendedProps.userName == userName) {
+          // add event to the calendar
+          calendarObjWrapper.calendar.addEvent(todo);
+          // add personalCategories to the list
+          if (!autocompleteOptions.includes(todo.extendedProps.category)) {
+            autocompleteOptions.push(todo.extendedProps.category);
+            renderCategorySelectOptions(autocompleteOptions);
+          }
+        }
+      }
+      // add personalCategories to the list
+      if (!autocompleteOptions.includes(todo.extendedProps.category)) {
+        autocompleteOptions.push(todo.extendedProps.category);
+        renderCategorySelectOptions(autocompleteOptions);
+      }
     }
   });
   listItemWrapper.addEventListener("dragover", initSortableList);
@@ -286,26 +430,6 @@ document
       } else if (color == "") {
         color = "#5bc0ff80";
       }
-
-      /*
-
-
- {
-  "id": "1694289073498",
-  "title": "say hi",
-  "allDay": true,
-  "start": "2023-09-09",
-  "end": "2023-09-09",
-  "classNames": [
-   "border-0"
-  ],
-  "backgroundColor": "#5bc0ff80",
-  "extendedProps": {
-   "isDone": false,
-   "userName": "Hesam"
-  }
-      */
-
       const todoObject = {
         id: Date.now(),
         title: title,
@@ -317,11 +441,13 @@ document
         extendedProps: {
           userName: userName,
           isDone: false,
+          category: categoryInput.value == "" ? "عمومی" : categoryInput.value,
         },
       };
       await postData(todoObject);
       event.target.closest(".inputWrapper").querySelector("input").value = "";
       event.target.closest(".inputWrapper").style.backgroundColor = "";
+      categoryInput.value = "";
       await renderItems();
     }
   });
